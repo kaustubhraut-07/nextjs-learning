d4502fd3384d6a054dcb7f87eef5e64d17247a33
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-09-09 11:07:31 +0530

    ssr and isr differances for rendering patterns

--- pages/todos.js
+++ pages/todos.js
@@ -1,28 +1,29 @@
-export default function todo({ posts }) {
-    {
-        posts?.todos?.map((todo)=>(<>
-         <div key={todo.id}>
-            <h4>
-                {todo.todo}
-                </h4>
-                {/* <p> */}
-                    {todo.completed === true ? 'true' : 'false'}
-                    {/* </p> */}
-                    </div>
-        </>))
-    }
+export default function Todo({ todos }) {
+    console.log(todos);
+  
+    return (
+      <div>
+        {todos?.map((todo) => (
+          <div key={todo.id}>
+            <h4>{todo.todo}</h4>
+            <p>{todo.completed === true ? 'Completed' : 'Not Completed'}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
-
-export async function getStaticProps() {
+  
+  export async function getStaticProps() {
     // Call an external API endpoint to get posts
-    const res = await fetch('https://dummyjson.com/todos')
-    const posts = await res.json()
-   console.log(posts)
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
+    const res = await fetch('https://dummyjson.com/todos');
+    const posts = await res.json();
+    const todos = posts.todos;
+    console.log(posts, "123");
+  
     return {
       props: {
-        posts,
+        todos,
       },
-    }
+    };
   }
+  
